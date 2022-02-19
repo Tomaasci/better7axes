e3acc8ce7faa1878f27c22c2348aa9ad31fc2410
_bannned_ <75911464+bannnedb@users.noreply.github.com>
2022-02-20 06:45:21 +1100

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,335 @@
+ideologies = [
+    {
+        "name": "Reactionary Centrism",
+        "stats": {
+            "econ": 50,
+            "dipl": 15,
+            "govt": 40,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Catholic Integralism",
+        "stats": {
+            "econ": 60,
+            "dipl": 25,
+            "govt": 30,
+            "scty": 10
+        }
+    },
+    {
+        "name": "Paleoconservatism",
+        "stats": {
+            "econ": 40,
+            "dipl": 20,
+            "govt": 30,
+            "scty": 20
+        }
+    },
+    {
+        "name": "National Syndicalism",
+        "stats": {
+            "econ": 75,
+            "dipl": 20,
+            "govt": 25,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Distributism",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 40,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Paternalistic Conservatism",
+        "stats": {
+            "econ": 65,
+            "dipl": 25,
+            "govt": 30,
+            "scty": 25
+        }
+    },
+    {
+        "name": "Right-Wing Populism",
+        "stats": {
+            "econ": 40,
+            "dipl": 25,
+            "govt": 40,
+            "scty": 25
+        }
+    },
+    {
+        "name": "Reactionary Liberalism",
+        "stats": {
+            "econ": 30,
+            "dipl": 15,
+            "govt": 60,
+            "scty": 15
+        }
+    },
+    {
+        "name": "Left-Wing Populism",
+        "stats": {
+            "econ": 70,
+            "dipl": 75,
+            "govt": 45,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Classical Liberalism",
+        "stats": {
+            "econ": 20,
+            "dipl": 40,
+            "govt": 65,
+            "scty": 50
+        }
+    },
+    {
+        "name": "English Socialism (IngSoc)",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Centrism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "FALGSC",
+        "stats": {
+            "econ": 100,
+            "dipl": 100,
+            "govt": 50,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Revolutionary Progressivism",
+        "stats": {
+            "econ": 70,
+            "dipl": 70,
+            "govt": 30,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Bolshevism",
+        "stats": {
+            "econ": 90,
+            "dipl": 80,
+            "govt": 20,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Stalinism",
+        "stats": {
+            "econ": 90,
+            "dipl": 30,
+            "govt": 10,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Trotskyism",
+        "stats": {
+            "econ": 90,
+            "dipl": 90,
+            "govt": 20,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Hive-Mind Collectivism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Fordism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Juche",
+        "stats": {
+            "econ": 90,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 40
+        }
+    },
+    {
+        "name": "Libertarian Socialism",
+        "stats": {
+            "econ": 75,
+            "dipl": 75,
+            "govt": 75,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Anarcho-Socialism",
+        "stats": {
+            "econ": 70,
+            "dipl": 80,
+            "govt": 90,
+            "scty": 80
+        }
+    },
+    {
+        "name": "Conservative Libertarianism",
+        "stats": {
+            "econ": 20,
+            "dipl": 30,
+            "govt": 70,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Anarcho-Capitalism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Neoconservatism",
+        "stats": {
+            "econ": 40,
+            "dipl": 40,
+            "govt": 35,
+            "scty": 35
+        }
+    },
+    {
+        "name": "Libertarian Communism",
+        "stats": {
+            "econ": 100,
+            "dipl": 75,
+            "govt": 75,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Anarcho-Communism",
+        "stats": {
+            "econ": 100,
+            "dipl": 75,
+            "govt": 100,
+            "scty": 75
+        }
+    },
+    {
+        "name": "National Socialism",
+        "stats": {
+            "econ": 50,
+            "dipl": 5,
+            "govt": 10,
+            "scty": 20
+        }
+    },
+    {
+        "name": "Social Democracy",
+        "stats": {
+            "econ": 60,
+            "dipl": 70,
+            "govt": 30,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Center-Libertarianism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 75,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Bleeding-Heart Libertarianism",
+        "stats": {
+            "econ": 60,
+            "dipl": 75,
+            "govt": 75,
+            "scty": 75
+        }
+    },
+    {
+        "name": "Egoism",
+        "stats": {
+            "econ": 40,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Anarcho-Primitivism",
+        "stats": {
+            "econ": 60,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Authoritarian Capitalism",
+        "stats": {
+            "econ": 25,
+            "dipl": 30,
+            "govt": 15,
+            "scty": 45
+        }
+    },
+    {
+        "name": "National Capitalism",
+        "stats": {
+            "econ": 5,
+            "dipl": 15,
+            "govt": 5,
+            "scty": 15
+        }
+    },
+    {
+        "name": "Theocratic Monarchism",
+        "stats": {
+            "econ": 65,
+            "dipl": 5,
+            "govt": 20,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Falangism",
+        "stats": {
+            "econ": 60,
+            "dipl": 10,
+            "govt": 20,
+            "scty": 10
+        }
+    }
+];
---
+++ questions.js
@@ -0,0 +1,633 @@
+questions = [
+    {
+        "question": "A progressive taxation system is preferable to a flat taxation system.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Regulations almost always improve the economy.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Excessive government intervention poses a threat to the economy.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "In a capitalist system, only those who work hard can succeed.",
+        "effect": {
+            "econ": -7,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Protectionism is sometimes necessary in trade.",
+        "effect": {
+            "econ": 7,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Private charity is a bad thing, as it prevents the masses from reaching the conditions that cause revolution.",
+        "effect": {
+            "econ": 15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The practice of usury should be banned.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Selfishness, when it relates to economy, should be rewarded.",
+        "effect": {
+            "econ": -15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is immoral for the government to allow people to be without medical care.",
+        "effect": {
+            "econ": 7,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The rich should pay the majority of their income in taxes.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Communism is an ideal that can never work in practice.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Privatisation is needed to make an economy thrive.",
+        "effect": {
+            "econ": -7,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Free trade is a bad policy.",
+        "effect": {
+            "econ": 7,
+            "dipl": -7,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The rich do not pay their fair share.",
+        "effect": {
+            "econ": -7,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Environmental regulations benefit society.",
+        "effect": {
+            "econ": 7,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I would support a moratorium on all immigration.",
+        "effect": {
+            "econ": 0,
+            "dipl": -15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is important to promote domestic industry through legislation",
+        "effect": {
+            "econ": 5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The European Parliament needs more power.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 5
+        }
+    },
+    {
+        "question": "Our nation is superior to all others.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Foreign aid is a waste of money.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Multiculturalism enriches all cultures.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Governmental policies do not need to be justified to foreigners.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Veterans do not deserve special privileges.",
+        "effect": {
+            "econ": 0,
+            "dipl": 7,
+            "govt": 0,
+            "scty": 7
+        }
+    },
+    {
+        "question": "Homogeneity is important in maintaining national stability.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": -5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "An ideal nation would avoid forming alliances.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "An international alliance of nations committed to mutual defense is something I would support.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "International law is important.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It doesn't bother me if an immigrant flies the flag of their home country rather than the national flag.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is very important to maintain foreign influence.",
+        "effect": {
+            "econ": 0,
+            "dipl": 7,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "International organisations that aim to combat climate change have a positive effect of the world.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "The government should be able to monitor potential terrorists.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A police force is a necessary part of a functioning society.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 7,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The right to peacefully protest should exist.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The right to own firearms should exist.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "A centralised government is ideal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The media should be influenced by, if not controlled by the state.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The very existence of the state is a threat to our liberty.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Direct democracy is an ideal form of government.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 2
+        }
+    },
+    {
+        "question": "The non-aggression principle should form the basis of all laws.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Nobody has the right to discriminate against anyone for believing in the wrong god.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 7
+        }
+    },
+    {
+        "question": "Sodomy should be illegal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -15
+        }
+    },
+    {
+        "question": "Adultery should be illegal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Racists should be publicly shamed and harassed.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 7,
+            "scty": 10
+        }
+    },
+    {
+        "question": "The presumption of innocence is very important to a just legal system.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 7,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The government shouldn't be able to put citizens to death.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Same-sex relationships are an abomination.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "You cannot be moral if you are not religious.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Interracial marriages should be looked down upon by society.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -15
+        }
+    },
+    {
+        "question": "Physician-assisted suicide should be legal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "There is nothing wrong with poligamy.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 12
+        }
+    },
+    {
+        "question": "Affirmative action for racial minorities is a good policy.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Secularism is evil.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -4,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Swearing and profanity should be looked down upon by society.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Science is more important than religion and tradition.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I have no moral objections to men wearing dresses in public.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Marijuana use should be illegal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 12,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Prostitution should be legal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Prematrital sex should be illegal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Society was better many years ago than it is now.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -12
+        }
+    },
+    {
+        "question": "Climate change is real.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 7
+        }
+    },
+    {
+        "question": "The fundamental flaw of western societies is that their people have lost faith in God.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I support affirmative action for the poor.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Witchcraft should be a punishable offense.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -7
+        }
+    },
+    {
+        "question": "We must liberate ourselves from the ways of the past.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "No religions are superior to others.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 15
+        }
+    },
+    {
+        "question": "Modern values are better than the values of the past.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 15
+        }
+    },
+    {
+        "question": "Abortions should be redily available.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 7
+        }
+    },
+    {
+        "question": "Women are better off as homemakers.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Feminism has made the world better.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "Certain races are inferior to others.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    }
+];
+
