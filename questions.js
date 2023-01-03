263d08d6eec7b6989b9d079c88fa82a9fac88cce
Tomassci <42543513+Tomaasci@users.noreply.github.com>
2023-01-03 20:36:05 +0100

    beginning new life

---
+++ questions.js
@@ -0,0 +1,21 @@
+questions = [
+     // The more positive, the more socialist
+     // The more positive, the more globalist
+     // The more positive, the more libertarian
+     // The more positive, the more progressive
+     // The more positive, the more jingoist
+     // The more positive, the more transhumanist
+     // The more positive, the more religious
+    {
+        "question": "A progressive taxation system is preferable to a flat taxation system.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+];
