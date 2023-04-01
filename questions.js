42522677bf5de9d0beb2bc3338db9c6c30b021f2
Tomassci <42543513+Tomaasci@users.noreply.github.com>
2023-04-01 15:14:19 +0200

    Added 1st set of questions.

--- questions.js
+++ questions.js
@@ -18,4 +18,328 @@ questions = [
             "r": 0
         }
     },
+    {
+        "question": "The freer the markets, the freer the people",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Economic inequality will be solved by free markets",
+        "effect": {
+            "econ": -20,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "The only way to stop economic exploitation is to hand factories to workers.",
+        "effect": {
+            "econ": 20,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "We should cooperate with countries that share our ideals.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "We should be skeptical of any foreign cooperation.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "I am sympathetic to various World Federalist Movements.",
+        "effect": {
+            "econ": 0,
+            "dipl": 20,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Globalism just removes countries' national sovereignty and thus should be opposed.",
+        "effect": {
+            "econ": 0,
+            "dipl": -20,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "If war can be avoided, it should be avoided.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": -10,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Military should be used if necessary.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 10,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "War is too looked down upon.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 20,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Total pacifism is realistic and desirable.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": -20,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "At times of necessity, security is more important than freedom.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": -10,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "The government has no business looking into most citizens' lives",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 10,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "There is such a thing as a morally correct dictator.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": -20,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "The state is best described as a monopoly on violence.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 20,
+            "scty": 0,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Our society should be more inclusive.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 10,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "In the modern times, openness has gone too far.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": -10,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Culturally we should return back to the ideals of the past.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": -20,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Discrimination is still too rampant.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 20,
+            "t": 0,
+            "r": 0
+        }
+    },
+    {
+        "question": "Technological progress for the past 10 years has been a force of good.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 10,
+            "r": 0
+        }
+    },
+    {
+        "question": "Thanks to technology, modern life is unfulfilling.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": -10,
+            "r": 0
+        }
+    },
+    {
+        "question": "We should seek to transform our bodies with the use of technology.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 20,
+            "r": 0
+        }
+    },
+    {
+        "question": "The Industrial revolution was a mistake.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": -20,
+            "r": 0
+        }
+    },
+    {
+        "question": "Religion is most of the time a force for good.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 10
+        }
+    },
+    {
+        "question": "Everyone should have a religion.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": 20
+        }
+    },
+    {
+        "question": "Atheism is the most logical belief.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": -10
+        }
+    },
+    {
+        "question": "Religion should be suppressed.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "w": 0,
+            "govt": 0,
+            "scty": 0,
+            "t": 0,
+            "r": -20
+        }
+    },
 ];
